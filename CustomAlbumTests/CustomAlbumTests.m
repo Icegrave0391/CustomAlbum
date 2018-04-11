5f90a20426bc2ce8e16432d4992f06f53f01e3e3
Icegrave0391 <849680403@qq.com>
2018-04-11 16:16:50 +0800

    Initial Commit

---
+++ CustomAlbumTests/CustomAlbumTests.m
@@ -0,0 +1,39 @@
+//
+//  CustomAlbumTests.m
+//  CustomAlbumTests
+//
+//  Created by 张储祺 on 2018/4/11.
+//  Copyright © 2018年 张储祺. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CustomAlbumTests : XCTestCase
+
+@end
+
+@implementation CustomAlbumTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
